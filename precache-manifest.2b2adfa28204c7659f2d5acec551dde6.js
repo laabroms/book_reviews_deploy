f39fa7c703d9ea5cc22682991cff9a53ab5d1680
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-09-29 20:00:24 -0700

    Updates

---
+++ precache-manifest.2b2adfa28204c7659f2d5acec551dde6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d079d616eeeca789a3ec24d11a43df70",
+    "url": "/book_reviews_deploy/index.html"
+  },
+  {
+    "revision": "43355000353f5436ebc2",
+    "url": "/book_reviews_deploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "870e696ac913fd5138c0",
+    "url": "/book_reviews_deploy/static/css/main.d56f4533.chunk.css"
+  },
+  {
+    "revision": "43355000353f5436ebc2",
+    "url": "/book_reviews_deploy/static/js/2.dd493545.chunk.js"
+  },
+  {
+    "revision": "95bb60cd5d6c9a5adb06ecf57a72f072",
+    "url": "/book_reviews_deploy/static/js/2.dd493545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870e696ac913fd5138c0",
+    "url": "/book_reviews_deploy/static/js/main.012599a8.chunk.js"
+  },
+  {
+    "revision": "d7f2be73856570350177",
+    "url": "/book_reviews_deploy/static/js/runtime-main.e8f332b5.js"
+  }
+]);
